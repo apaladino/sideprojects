6122e0cec2bf048a333e2d5e03e5591542de3012
Andy Paladino <andy_paladino@yahoo.com>
2020-11-17 21:41:04 -0500

    added bootstrap config

--- django/myapp/dbutil/views.py
+++ django/myapp/dbutil/views.py
@@ -5,17 +5,37 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
-
+class Database:
+    def __init__(self, name, title):
+        self.name = name
+        self.title = title
 
 dbutil_title='DBA Utilities App'
 dbutil_welcome_msg='Welcome to the DBA Utilities App'
+create_user_welcome_msg='Create User Like Existing User'
+sub_title="Andy's Automation Center"
+databases = [ Database("dev", "Development"), Database("staging", "Staging"),
+              Database("prod", "Production"), Database("meta", "Meta")]
+
 # Create your views here.
 def index(request):
-    context = { 'title': dbutil_title, 'welcome_msg': dbutil_welcome_msg}
+    context = {
+        'title': dbutil_title,
+        'welcome_msg': dbutil_welcome_msg,
+        'sub_title': sub_title,
+        'media_heading': 'Main Page'}
     template = loader.get_template('dbutil/index.html')
 
     return HttpResponse(template.render(context, request))
 
 
 def create_user_like_existing_user(request):
-    return HttpResponse("Create user like existing user")
+    context = {
+        'title': dbutil_title,
+        'welcome_msg': create_user_welcome_msg,
+        'sub_title': sub_title,
+        'media_heading': 'Create User',
+        'databases' : databases}
+    template = loader.get_template('dbutil/createusers.html')
+
+    return HttpResponse(template.render(context, request))
